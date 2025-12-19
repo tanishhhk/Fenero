ad01a1bfc655c18d4bd862bed116679f686b88ed
Tanishk <searchforthefox@gmail.com>
2025-12-19 11:49:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
 // src/App.jsx
+
+/**
+ * Copyright (c) 2025-2026 Fenero Capital Advisory LLP
+ * All Rights Reserved.
+ * 
+ * This file is part of the Fenero platform and is proprietary software.
+ * Unauthorized copying, modification, or distribution is strictly prohibited.
+ * 
+ * Author: Tanishk Jain
+ * Company: Fenero Capital Advisory LLP
+ * Contact: fenerocapitaladvisory@gmail.com
+ */
+
 import { Routes, Route, useLocation } from "react-router-dom";
 import AdminDashboard from './components/AdminDashboard';
 import { useEffect } from "react";
